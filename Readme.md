84f4df8bd0432e818a6012a0da65897fc5e2781d
George Snelling <georgesn@gmail.com>
2013-10-27 23:19:08 -0700

    initial commit

---
+++ Readme.md
@@ -0,0 +1,250 @@
+#scrub [![NPM version](https://badge.fury.io/js/scrub.png)](http://badge.fury.io/js/scrub)
+
+  An experimental recursive syncronous javascript value scrubber forked from chk.
+
+## Why Scrub?
+  Scrub aims to solve this problem:
+```js
+function fn(v) {
+  // what the heck is v?
+}
+```
+## Install for nodejs
+```
+npm install scrub
+```
+### Basic Use
+```js
+var scrub = require('scrub')
+
+function fn(v) {
+
+  var spec = {type: 'string'}
+  var err = scrub(v, spec)
+  if (err) return err
+
+  // I know everything I need to know about v
+  ...
+}
+fn(1)         // Error: 'Invalid type'
+fn('foo')     // 3
+```
+With scrub I define all my assumptions about a value with a succint spec.  If the value fails my spec, scub crafts a detailed error explaining where things went wrong.
+
+### Scrub specs
+Scrub specs are ordinary objects that you define. Here is the bootstrap spec for a spec. The most important properties are type, value, required, and default.  Scrub recurses on the value property for nested specs, and iterates the scrub over arrays.
+```js
+var _spec = {
+  type: 'object',
+  value: {
+    init:       {type: 'function'},
+    default:    {},
+    required:   {type: 'boolean'},
+    type:       {type: 'string'},
+    value:      {},
+    validate:   {type: 'function'},
+    finish:     {type: 'function'},
+  }
+}
+```
+
+### Default
+```js
+spec = {
+  s1: {type: 'string'}
+  s2: {type: 'string', default: 'goodbye'}
+}
+val = {s1: 'hello'}
+err = scrub(val, spec)  // null
+console.log(val)        // {s1: 'hello', s2: 'goodbye'}
+```
+
+### Required
+```js
+spec = {s1: {type: 'string', required: true}}
+scrub({s1: 'foo', s2: 'bar'}, spec)   // null
+scrub({s2: 'bar'}, spec)              // Error with code 'missingParam'
+```
+
+### Type
+The type property is string with the following supported values:
+
+    'undefined'
+    'null'
+    'string'
+    'boolean'
+    'number'
+    'object'
+    'array'
+    'error'
+    'function'
+    'arguments'
+
+
+Specify multiple valid types for a value like so:
+
+    type: 'string|number|boolean'
+
+The type of any value is determied by the tipe module: https://github.com/3meters/tipe.  Tipe supports custom types, enabling you to extend the type list.
+
+```js
+spec = {
+  str1: {type: 'string'},
+  num1: {type: 'number'}
+}
+err = scrub({str1: 'foo', num1: 2}, spec)  // null
+err = scrub({str1: 2}, spec)               // Error with code 'badType'
+```
+
+### Value checking with delimted string enums
+```js
+spec = {type: 'string', value: 'one|or|another'}
+scrub('or', spec)      // null
+scrub('notOne', spec)  // Error wtih code 'badValue'
+```
+
+### Optionally fail on unknown object keys with option strict
+```js
+spec = {foo: {type: 'string'}}
+scrub({foo: 'hello', bar: 'goodbye'}, spec)  // null
+scrub({foo: 'hello', bar: 'goodbye'}, spec, {strict: true})  // Error with code 'badParam'
+```
+
+### Custom Setters
+The init, default, value, and finish properties of a spec accept a setter function.  Setters are passed the curent value being scrubed and the options object.  Setters should return the new value or an error.
+```js
+spec = {n1: {
+  type: 'number',
+  default: function(v, options) {
+    if (options.foo) return 2
+    else return 1
+  },
+  value: function(v, options) {
+    return ({n1: v[n1], n2: v[n1] * v[n1]})
+  }
+}}
+val = {}
+err = scrub({}, spec}             // null
+val                                   // {n1:1, n2:1}
+err = scrub({}, spec, {foo: true}     // null
+val                                   // {n1:2, n2:4}
+```
+
+### Custom Validators
+Validators are similar to setters, except they return a positive value on failure, in most cases a simple string. Scrub will convert a string returned by validator into an error with the code property set to 'badValue'.  To override this code, return an Error with its code property set.  
+```js
+spec = {
+  type: 'number',
+  validate: function(v, options) {
+    if (v < 0) return 'n1 must be greater than zero'
+  }
+}}
+
+scrub({n1: 1}, spec)   // null
+scrub({n1: -1}, spec)  // Error: 'n1 must be greater than zero', Error.code: 'badValue'
+```
+or
+```js
+spec = {
+  type: 'number',
+  validate: function(v, options) {
+    if (v < 0) {
+      err = new Error('n1 must be greater than zero')
+      err.code = 'i_died'
+      return err
+    }
+  }
+}}
+
+scrub({n1: 1}, spec)   // null
+scrub({n1: -1}, spec)  // Error: 'n1 must be greater than zero', Error.code: 'i_died'
+```
+### Passing Data to Setters and Validators
+Passing data to validators or setters is easy. Within your validator or setter, the this object refers to the top-level passed-in value, and any data you pass in on the options object is passed through.
+```js
+  spec = {
+    n1: {type: 'number', default: 0},
+    n2: {type: 'number', validate: n2Validate}
+  }
+  function n2Validate(v, options) {
+    if (v !== this.n1) return 'n2 must equal n1'
+    if (options.foo) return 'options.foo must not be'
+  }
+  scrub({n1:1, n2:1}, spec)  // null
+  scrub({n1:1, n2:2}, spec)  // Error: 'n2 must equal n1'
+  scrub({n1:1, n2:1}, spec, {foo:true})  // Error: 'options.foo must not be'
+
+```
+### Type coersion
+For values with exected types of number or boolean, when fed a value of type string, by default scub will try to coerce the string to the spec'ed type before evalutating, converting '1' to 1 for a number, and 'true' to true for a boolean.  This is handy for accepting web query strings.  To turn this behavior off at any level, set options.doNotCoerce to true.
+```js
+spec = {n1: {type: 'number'}, b1: {type: 'boolean'}}
+val = {n1: '12', b2: 'true'}
+err = scrub(val, spec)  // null
+val                         // {n1: 12, b2: true}  types have been coerced
+val = {n1: '12', b2: 'true'}
+err = scrub({n1: '12', b2: 'true'}, spec, {doNotCoerce: true}) // Error with code 'badType'
+```
+
+### Arrays
+For arrays Scrub tests each element in the array against the spec
+```js
+spec = {a1: {type: 'array', value: {type: 'number'}}}
+err = scrub({a1: [1,2,3]})  // err is null
+err = scrub({a1: [1, 2, '3']})  // err is Error with code 'badType'
+```
+or with an array of objects
+```js
+spec = {
+  {type: 'array' value: {type: 'object', value: {s1: {type: 'string'}, n1: {type: 'number'}}}
+}
+err = scrub([{s1: 'foo', n1: 1}, {s1: 'bar', n1: 2}])  // err is null
+err = scrub([{s1: 'foo', n1: 1}, {s1: 'bar', n1: 'baz'}])  // err is Error with code 'badType'
+```
+### Options
+The top level scurb call accepts
+```js
+scrub(val, spec, options)
+```
+Where options is a non-strict object with following supported properties and their defaults:  
+```js
+  {
+    strict: false,          // do not allow unspecified properties of objects
+    ignoreDefaults: false,  // do not set default values, handy for db updates
+    ignoreRequired: false,  // do not enforce required, handy for db updates
+    doNotCoerce: false,     // do not coerce types
+    returnValue: false,     // return the scrubed value, rather than null, on success
+                            //    handy for scrubing scalars or for recasting objects
+    log: false              // log the arguments to each recursive scrub call,
+                            //    handy for debugging deeply nested spec
+  }
+```
+Options can be set as an optional third argument to the top level call, or as properties of any spec or sub-spec.  They remain set for all children unless they are overridden.  For example, a top-level spec can be strict, meaning no unrecognized properties are allowed, except for one property, which can be unstrict, allowing un-specified sub-properties, except for one of its sub-properties, which must be strict, etc. For example: 
+```js
+spec = {
+  o1: {type: 'object', value: {
+    s1: {type: 'string'},
+    s2: {type: 'string'},
+  },
+  o2: {type: 'object', strict: false, value: {
+    s1: {type: 'string'},
+    s2: {type: 'string'},
+    o1: {type: 'object', strict: true: value: {
+      n1: {type: 'number'}
+    }
+  }
+}
+val = {
+  o1: {s1: 'foo', s2: 'bar'},
+  o2: {s1: 'foo', s2: 'bar', s3: 'baz}
+}
+err = scrub(val, spec, {strict: true}) // err is null because o2 strict attribute overrode option
+val.o2.o1 = {n2: 100}
+err = scrub(val, spec, {strict: true}) // err is Error because spec.o2.o1 does not allow properties other than n1
+```
+## Contributing
+Contributions welcome.
+## Copyright
+  Copyright (c) 2013 3meters.  All rights reserverd.
+## License
+  MIT
---
+++ test.js
@@ -0,0 +1,577 @@
+/**
+ * scrub tests
+ *
+ * tests are synchronous and throw on first failure
+ */
+
+
+// Dependencies
+var scrub = require('./scrub')
+var tipe  = require('tipe')        // https://github.com:3meters/tipe
+var _assert = require('assert')
+var inspect = require('util').inspect
+
+
+// Module vars
+var isError = tipe.isError
+var isNull = tipe.isNull
+var cAsserts = 0
+var test = {}
+var val, spec, err   // reset for each test
+
+
+// Add corpse to failures
+function assert(expr) {
+  _assert.call(null, expr, 'test failed\ndump: \n' +
+      inspect({val: val, spec: spec, err: err}, {depth:10}))
+  cAsserts++
+}
+
+
+// Run tests
+function main() {
+  console.log('\nscrub tests\n===========')
+  for (var t in test) {
+    console.log(t)
+    var val, spec, err  // reset module vars
+    test[t]()
+  }
+  console.log('\nscrub ok')
+}
+
+
+// Tests
+
+test.minimalWorks = function() {
+  spec = {type: 'number', required: true}
+  err = scrub(1, spec)
+  assert(isNull(err))
+  err = scrub('foo', spec)
+  assert(isError(err))
+  assert('badType' === err.code)
+}
+
+
+test.basicRequired = function() {
+  err = scrub(undefined, {required: true})
+  assert(isError(err))
+  assert('missingParam' === err.code)
+}
+
+
+test.basicDefault = function() {
+  val = {}
+  err = scrub(val, {n1: {default: 1}})
+  assert(isNull(err))
+  assert(1 === val.n1)
+}
+
+
+test.basicArray = function() {
+  spec = {type: 'array', value: {type: 'string'}}
+  val = []
+  err = scrub(val, spec)
+  assert(isNull(err))
+  val = ['foo', 'bar', 'baz']
+  err = scrub(val, spec)
+  assert(isNull(err))
+  val.push(1)
+  err = scrub(val, spec)
+  assert(isError(err))
+  assert('badType' === err.code)
+  assert(err.details)
+  assert(err.details.rootSpec)
+  assert('array' === err.details.rootSpec.type)
+  assert(err.details.options)
+  assert(3 === err.details.key)
+  assert(tipe.isUndefined(err.details.options.strict))
+}
+
+
+test.bigSuccedes = function() {
+  spec = {
+    s1: { type: 'string', required: true },
+    s2: { type: 'string', default: 'hi' },
+    o1: { type: 'object', value: {
+      s1: { type: 'string' },
+      s2: { type: 'string', default: 'hi' },
+      s3: { type: 'string', default: '' },
+      n1: { type: 'number' },
+      n2: { type: 'number', default: 1 },
+      n3: { type: 'number', default: 0 },
+      b1: { type: 'boolean' },
+    }},
+    o2: {
+      type: 'object', value: {
+        no1: {type: 'object', value: {
+          s1: { type: 'string', value: 'foo'}
+        }}
+      }
+    },
+    o3: {type: 'object', required: true, value: {
+        s2: {type: 'string', required: true},
+        a3: {type: 'array', required: true, value: {type: 'string'}}
+      },
+    },
+    a1: {type: 'array', value: {type: 'string'}},
+    a2: {type: 'array', value: {type: 'object', value: {
+            s1: {type: 'string', required: true},
+            s2: {type: 'string', default: 'hello'},
+        }}
+    },
+  }
+  val = {
+    s1: 'hello',
+    o1: { s1: 'foo', n1: 1, b1: true },
+    o2: { no1: { s1: 'foo', } },
+    o3: {s2: 'bla', a3: ['aaa', 'bbb', 'ccc']},
+    a1: ['123', '456', '789'],
+    a2: [{s1: 'foo'}, {s1: 'bar'}, {s1: 'baz'}, {s1: 'barney'}],
+  }
+  err = scrub(val, spec, {strict: true})
+  assert(isNull(err))
+  assert(val.s2)
+  assert('hi' === val.s2)
+  assert(val.o1)
+  assert('hi' === val.o1.s2)
+  assert(1 === val.o1.n2)
+  assert(0 === val.o1.n3)
+  assert(4 === val.a2.length)
+  val.a2.forEach(function(elm) {
+    assert(tipe.isString(elm.s1))
+    assert('hello' === elm.s2)
+  })
+  val.a2.push({s2: 'I should fail'})
+  err = scrub(val, spec)
+  assert(err)
+  assert('missingParam' === err.code)
+}
+
+
+test.coerceStrings = function() {
+  spec = {
+    n1: {type: 'number'},
+    n2: {type: 'number'},
+    n3: {type: 'number'},
+    n4: {type: 'number'},
+    n5: {type: 'number'},
+    b1: {type: 'boolean'},
+    b2: {type: 'boolean'},
+    b3: {type: 'boolean'},
+    b4: {type: 'boolean'},
+    b5: {type: 'boolean'},
+  }
+  val =  {
+    n1: '100',
+    n2: '-1',
+    n3: '0.52',
+    n4: '1.7',
+    n5: '0',
+    b1: 'true',
+    b2: 'foo',
+    b3: '1',
+    b4: '0',
+    b5: '-1',
+  }
+  err = scrub(val, spec)
+  assert(isNull(err))
+  assert(100 === val.n1)
+  assert(-1 === val.n2)
+  assert(0.52 === val.n3)
+  assert(1.7 === val.n4)
+  assert(0 === val.n5)
+  assert(true === val.b1)
+  assert(false === val.b2)
+  assert(true === val.b3)
+  assert(false === val.b4)
+  assert(false === val.b5)
+}
+
+
+test.undefinedValuesPassTypeCheck = function() {
+  err = scrub({s1: undefined}, {s1: {type: 'string'}})
+  assert(isNull(err))
+}
+
+
+test.missingRequiredScalar = function() {
+  spec =  {type: 'number', required: true}
+  val = undefined
+  err = scrub(val, spec)
+  assert(isError(err))
+  assert('missingParam' === err.code)
+}
+
+
+test.missingRequiredObject = function() {
+  spec = {
+    s1: {type: 'string'},
+    o1: {type: 'object', required: true}
+  }
+  val = { s1: 'foo' }
+  err = scrub(val, spec)
+  assert(isError(err))
+  assert('missingParam' === err.code)
+}
+
+
+test.missingRequiredNested = function() {
+  spec = {
+    s1: {type: 'string'},
+    o1: {type: 'object', required: true, value: {
+        s1: {type: 'string', required: true}
+      }
+    }
+  }
+  val = {
+    s1: 'foo',
+    o1: {s1: 'I am nested s1'}
+  }
+  err = scrub(val, spec)
+  assert(!err)
+  val = {
+    s1: 'foo',
+    o1: {s2: 'I am not nested s1'}
+  }
+  err = scrub(val, spec)
+  assert(isError(err))
+  assert('missingParam' === err.code)
+}
+
+
+test.topLevelArrays = function() {
+  spec = {type: 'array', value: {
+    type: 'object', value: {
+      n1: {type: 'number', required: true},
+      s1: {type: 'string', default: 'foo'}
+    }
+  }}
+  val = [{n1: 1}, {n1: 2}]
+  err = scrub(val, spec)
+  assert(!err)
+  assert('foo' === val[0].s1)
+  assert('foo' === val[1].s1)
+  val = [{n1: 1}, {s1: 'bar'}]
+  err = scrub(val, spec)
+  assert(isError(err))
+  assert('missingParam' === err.code)
+}
+
+
+test.strictWorks = function() {
+  spec = {
+    s1: {type: 'string'},
+    o1: {type: 'object', required: true, value: {
+        s1: {type: 'string', required: true}
+      }
+    },
+  }
+  val = {
+    s1: 'foo',
+    o1: {
+      s1: 'I am required',
+      s2: 'I am not allowed with strict'
+    }
+  }
+  err = scrub(val, spec)
+  assert(isNull(err))
+  err = scrub(val, spec, {strict: true})
+  assert(isError(err))
+  assert('badParam' === err.code)
+  assert(err.details)
+  assert(err.details.options)
+  assert(err.details.options.strict)
+}
+
+
+test.nestedStrictWorks = function() {
+  spec = {
+    s1: {type: 'string'},
+    o1: {
+      type: 'object',
+      required: true,
+      strict: false,
+      value: {s1: {type: 'string', required: true}}
+    }
+  }
+  val = {
+    s1: 'foo',
+    o1: {
+      s1: 'I am required',
+      s2: 'I am not allowed with strict'
+    }
+  }
+  err = scrub(val, spec)
+  assert(isNull(err))
+  err = scrub(val, spec, {strict: true})
+  assert(isNull(err))  // local option overroad global options
+  spec.o1.strict = true
+  err = scrub(val, spec)
+  assert(isError(err))
+  assert('badParam' === err.code)
+}
+
+test.arrayTypesPass = function() {
+  spec = {
+    a1: {type: 'array', value: {type: 'string'}},
+    a2: {type: 'array', value: {type: 'object', value: {
+            s1: {type: 'string', required: true},
+        }}
+    },
+    o1: {type: 'object', required: true, value: {
+        s2: {type: 'string', required: true},
+        a3: {type: 'array', required: true, value: {type: 'string'}}
+      }
+    },
+  }
+  val = {
+    a1: ['123', '456', '789'],
+    a2: [{s1: 'foo'}, {s1: 'bar'}, {s1: 'baz'}],
+    o1: {s2: 'bla', a3: ['aaa', 'bbb', 'ccc']},
+  }
+  var options = { strict: true }
+  err = scrub(val, spec, options)
+  assert(isNull(err))
+}
+
+
+test.arrayBasicFailsProperly = function() {
+  spec = {
+    a1: {type: 'array', value: {type: 'string'}, required: true}
+  }
+  val = { a1: ['123', '456', '789', 11] }
+  var options = { strict: true }
+  err = scrub(val, spec, options)
+  assert(isError(err))
+  assert('badType' === err.code)
+}
+
+
+test.enumsWork = function() {
+  spec = {s1: {type: 'string', value: 'foo|bar|baz'}}
+  val = {s1: 'bar'}
+  err = scrub(val, spec)
+  assert(isNull(err))
+  err = scrub({s1: 'notfoo'}, spec)
+  assert(isError(err))
+  assert('badValue' === err.code)
+}
+
+
+test.valueSettersWorkForScalars = function() {
+  spec = {type: 'object', value: {
+    s1: {
+      type: 'string',
+      value: function(v) {
+        return (v+v)
+      }
+    }
+  }}
+  var o = {s1: 'foo'}
+  err = scrub(o, spec)
+  assert(isNull(err))
+  assert('foofoo' === o.s1)
+}
+
+
+test.valueSettersWorkForObjects = function() {
+  spec = {type: 'object',
+    value: function(o) {
+      for (var key in o) {
+        o[key] = o[key] + 'bar'
+      }
+      return o
+    }
+  }
+  var o = {s1: 'bar'}
+  err = scrub(o, spec)
+  assert(isNull(err))
+  assert('barbar' === o.s1)
+}
+
+test.valueSettersWorkForArrays = function() {
+  spec = {
+    type: 'array',
+    value: {
+      type: 'object',
+      value: function(o) {
+        for (var key in o) {
+          o[key] = o[key] + 'bar'
+        }
+        return o
+      }
+    }
+  }
+  var a = [{s1: 'foo'}, {s1: 'bar'}]
+  err = scrub(a, spec)
+  assert(isNull(err))
+  assert('foobar' === a[0].s1)
+  assert('barbar' === a[1].s1)
+}
+
+test.valueSettersThatThrowReturnBadSchemaError = function() {
+  spec = {
+    type: 'object', value: function(v) {
+      throw new Error('I hurled')
+    }
+  }
+  err = scrub({}, spec)
+  assert(isError(err))
+  assert('badSpec' === err.code)
+}
+
+test.validatorsWork = function() {
+  spec = {
+    type: 'string',
+    validate: function(v) {
+      if (v[0] && v[0] !== v[0].toUpperCase()) {
+        return 'Must be uppercase'
+      }
+    }
+  }
+  err = scrub('Hello', spec)
+  assert(!err)
+  err = scrub('hello', spec)
+  assert(isError(err))
+  assert('badValue' === err.code)
+}
+
+
+test.namedValidatorsWork = function() {
+  function valid(v) {
+    if (v < 1) return 'fail'
+  }
+  spec = {
+    validate: valid
+  }
+  err = scrub(0, spec)
+  assert(isError(err))
+  err = scrub(1, spec)
+  assert(isNull(err))
+}
+
+
+test.functionValidatorsWorkWithCustomErrorCodes = function() {
+  spec = {
+    type: 'string',
+    value: function(v) {
+      // true if first char is uppercase
+      if (v[0] && v[0] !== v[0].toUpperCase()) {
+        err = new Error('Must be uppercase')
+        err.code = 'mustBeUppercase'
+        return err
+      }
+    }
+  }
+  err = scrub('Hello', spec)
+  assert(!err)
+  err = scrub('hello', spec)
+  assert(isError(err))
+  assert('mustBeUppercase' === err.code)
+}
+
+
+test.functionValidatorsGiveSpecErrIfTheyThrow = function() {
+  spec = {
+    type: 'string',
+    value: function(v) {
+      foo.bar // will throw a runtime error
+    }
+  }
+  err = scrub('Hello', spec)
+  assert(isError(err))
+  assert('badSpec' === err.code)
+}
+
+
+
+test.validatorsCanAccessThisObjectAndUserDefinedOptions = function() {
+  spec = {
+    n1: {type: 'number', default: 0},
+    n2: {type: 'number', validate: n2Validate}
+  }
+  function n2Validate(v, options) {
+    if (v !== this.n1) return 'n2 must equal n1'
+    if (v !== options.n3)  return 'n2 must equal options.n3'
+  }
+  err = scrub({n1:1, n2:1}, spec, {n3: 1})
+  assert(isNull(err))
+  err = scrub({n1:1, n2:2}, spec, {n3: 1})
+  assert(isError(err))
+  assert('badValue' === err.code)
+  assert('n2 must equal n1' === err.message)
+  err = scrub({n1:2, n2:2}, spec)
+  assert(isError(err))
+  assert('n2 must equal options.n3' === err.message)
+  err = scrub({n1:2, n2:2}, spec, {n3: 3})
+  assert(isError(err))
+  assert('n2 must equal options.n3' === err.message)
+}
+
+
+test.validatorsWithArrays = function() {
+  function valid(v) {
+    if (v.n1 <= v.n2) {
+      err = new Error('n1 must be greater than n2')
+      err.code = 'failedValidator'
+      return err
+    }
+    return null
+  }
+  spec = {
+    type: 'array', value: {
+      type: 'object', value: {
+        n1: {type: 'number'},
+        n2: {type: 'number'},
+      },
+      validate: valid
+    }
+  }
+  val = []
+  err = scrub(val, spec)
+  assert(isNull(err))
+  val.push({n1: 2, n2: 1})
+  err = scrub(val, spec)
+  assert(isNull(err))
+  val.push({n1: 1, n2: 2})
+  err = scrub(val, spec)
+  assert(isError(err))
+  assert('failedValidator' === err.code)
+}
+
+test.initAndFinishWork = function() {
+  err, spec = {
+    init: function(v, options) {
+      if ('object' === tipe(v)) {
+        options.initRan = true
+        return [v]
+      }
+      else return (v)
+    },
+    type: 'array',
+    finish: function(v, options) {
+      if (options.initRan) {
+        v.push({s1: 'bar'})
+      }
+      return v
+    }
+  }
+  val = {s1: 'foo'}
+  val = scrub(val, spec, {returnValue: true})
+  assert(!isError(val))
+  assert(2 === val.length)
+  assert('bar' === val[1].s1)
+}
+
+test.specsCanHaveExtraFields = function() {
+  spec = {
+    s1: {type: 'string', foo: 'bar'}
+  }
+  val = {s1: 'hello'}
+  err = scrub(val, spec)
+  assert(isNull(err))
+}
+
+
+// Run tests
+main()
